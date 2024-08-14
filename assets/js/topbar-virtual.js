5e9bf76dccce6ab2276740af072945b6a92ef0d6
kietttv <tvtkiet2002@gmail.com>
2024-08-14 08:30:03 +0700

    update modal - add check icon - fix some fail

--- assets/js/topbar-virtual.js
+++ assets/js/topbar-virtual.js
@@ -329,16 +329,19 @@ $(document).ready(function () {
       mess: message
     };
 
-    // postGoogleFrom(data);
+    postGoogleFrom(data);
     showModal();
   });
 
   function showModal() {
     const popupModal = `<span class="overlay"></span>
                         <div class="modal-box">
-                          <i class="fa-regular fa-circle-check"></i>
+                        	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+							              <circle class="path circle" fill="none" stroke="#198754" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1" />
+							              <polyline class="path check" fill="none" stroke="#198754" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 " /> 
+						              </svg> 
                           <h2>Completed</h2>
-                          <h3>You have sucessfully downloaded all the source code files.</h3>
+                          <h3>You have successfully sent me a message, I will respond to you as soon as possible.</h3>
                           <div class="buttons">
                             <button class="close-btn">Ok, Close</button>
                           </div>
